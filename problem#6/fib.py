c3dcecc8597dd2ef0819561dce8c276322c23426
Wyatt62902 <156848807+Wyatt62902@users.noreply.github.com>
2024-02-29 14:37:09 -0600

    problem 6

--- problem#6/fib.py
+++ problem#6/fib.py
@@ -1,7 +1,23 @@
+'''
+Name: Wyatt Fulton
+Lab Time: Thur 2:00PM
+'''
 def fibonacci(n):
-    #write your code here
+    flist = [0,1]
+    if n == 0:
+        output = 0
+    elif n < 0:
+        output = -1
+    else:
+        flist = flist*n
+        for i in range(2, n, 1):
+            flist[i] = flist[i-1]+flist[i-2]
+        output = n-1
+    return output
+
 
 
 if __name__ == '__main__':
     start_num = int(input())
     print(f'fibonacci({start_num}) is {fibonacci(start_num)}')
+    
