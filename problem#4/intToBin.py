82a818828b99755cf13263b7bbb4c85c1d7402d5
Wyatt62902 <156848807+Wyatt62902@users.noreply.github.com>
2024-02-29 13:34:12 -0600

    problem 5

--- problem#4/intToBin.py
+++ problem#4/intToBin.py
@@ -1,17 +1,16 @@
+
 def int_to_reverse_binary(num1):
     binary_val = ''
-#write your while loop here
     while num1 > 0:
-        #write your code
+        binary_val = binary_val + str(num1 % 2)
+        num1 = num1 // 2
 
-    return binary_val;
+    return binary_val
 
 
 def string_reverse(input_string): 
     reverse_input = ''
-    
-   #write your for loop here
-    
+    reverse_input = input_string [::-1]
     return reverse_input
 
 if __name__ == '__main__':
@@ -19,5 +18,9 @@ if __name__ == '__main__':
     
     binary_string = str(int_to_reverse_binary(user_input))
     binary_string = str(string_reverse(binary_string))
-    
     print(binary_string)
+
+'''
+Name: Wyatt Fulton
+Lab Time: Thur 2:00PM
+'''
