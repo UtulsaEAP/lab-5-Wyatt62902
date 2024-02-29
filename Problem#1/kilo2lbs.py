d44de3d156446e52991d6648edb2c70bdd956356
Wyatt62902 <156848807+Wyatt62902@users.noreply.github.com>
2024-02-29 12:37:17 -0600

    problem 1]

--- Problem#1/kilo2lbs.py
+++ Problem#1/kilo2lbs.py
@@ -1,6 +1,6 @@
 def kilo_to_pounds(kilos):
     # This statement intentionally has an error. 
-    return (kilos / 2.204)
+    return (kilos * 2.204)
 
 
 # Main part of the program starts here. Do not remove the line below.
@@ -9,3 +9,8 @@ if __name__ == '__main__':
     
     pounds = kilo_to_pounds(kilos)
     print(f'{pounds:.3f} lbs')
+
+
+
+# Wyatt Fulton
+# Thur 2:00 PM
